45cc1380db3234c189420ed070d839256bc4c7d0
rich <rich.p.martin@gmail.com>
2021-07-19 16:02:02 -0700

    some plots

--- code.R
+++ code.R
@@ -1,1 +1,13 @@
+library("tidyverse")
+library("MASS")
+ggplot(data=Boston, aes(x=nox,y=medv,colour=ptratio))+
+  geom_point()+
+  geom_smooth(method="lm")+
+  scale_colour_viridis_c()
 
+ggplot(data=Boston,aes(x=factor(chas,labels=c("no","yes")),y=medv))+
+  geom_boxplot(outlier.shape=NA,fill="red",alpha=.25)+
+  geom_jitter(alpha=.25)+
+  labs(x="Close to Charles River",
+       y="House prices in 1000's (1970)",
+       title="Seems like people like rivers")
