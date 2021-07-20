6ee6b563dceb56a452dfe2c4b00313f10c26c011
rich <rich.p.martin@gmail.com>
2021-07-19 18:31:17 -0700

    small changes

--- code.R
+++ code.R
@@ -1,9 +1,13 @@
 library("tidyverse")
 library("MASS")
-ggplot(data=Boston, aes(x=nox,y=medv,colour=ptratio))+
+ggplot(data=Boston, aes(x=nox,y=medv,colour=zn))+
   geom_point()+
   geom_smooth(method="lm")+
-  scale_colour_viridis_c()
+  scale_colour_viridis_c()+
+  labs(x="Nitrogen oxide levels (pp10m)",
+       y="House prices in 1000's (1970)",
+       colour="proportion of residential lots greater than 25,000 sq.ft.",
+       title="Lot size and Nitrogen oxide levels confounded.")
 
 ggplot(data=Boston,aes(x=factor(chas,labels=c("no","yes")),y=medv))+
   geom_boxplot(outlier.shape=NA,fill="red",alpha=.25)+
